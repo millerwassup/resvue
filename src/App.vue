bb7a012cb1adafabb916d8ec168a9ab32d6da2b5
Arthur Kasumyan <arthurkasumyan@gmail.com>
2019-11-12 23:00:43 -0500

    Make exercise 1, start 2

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
+      <v-container>
+        <v-layout justify-center wrap>
+          <router-link class="mx-1" to="/">1. Basic Variables</router-link>
+          <router-link class="mx-1" to="/exercise-two">2. Using Buttons and v-for</router-link>
+        </v-layout>
+      </v-container>
       <router-view/>
     </v-content>
   </v-app>
---
+++ src/views/ExerciseTwo.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container id="exercise-two">
+    <v-row no-gutters>
+      <v-col cols="12">
+        <h1>Exercise 2: Using buttons and v-for</h1>
+      </v-col>
+
+      <v-col cols="12">
+        <p>Edit the HTML tags and javascript to change the appearance and functionality.</p>
+      </v-col>
+    </v-row>
+
+    <v-row dense>
+      <v-col cols="12">
+        <h2>Part 1</h2>
+        Play around with the text field a little.
+      </v-col>
+
+      <v-col cols="12">
+        <b>Value of students:</b> {{ students }}
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12">
+        <h2>Part 5</h2>
+        How does the rendered HTML change when we use v-show instead of v-if?
+      </v-col>
+    </v-row>
+
+    <v-divider class="my-4"/>
+    <v-row>
+      <v-col cols="12">
+        <h2>Part 6</h2>
+        Add an HTML element using v-else-if that will pop up if msg is "Vue.js kinda rocks!"
+      </v-col>
+    </v-row>
+
+    <v-divider class="my-4"/>
+    <v-row>
+      <v-col cols="12">
+        <h2>Part 7</h2>
+        Add an HTML element using v-else that will pop up if msg is anything else.
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ExerciseTwo',
+
+  data: () => ({
+    students: []
+  })
+}
+</script>
